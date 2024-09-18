769802ee58439d591d97a6b46d964fed861c31bc
DavidIsrael <davidisrael.rj@gmail.com>
2024-09-18 10:40:56 -0300

    userController

---
+++ src/user/user.controller.ts
@@ -0,0 +1,10 @@
+import {Controller, Post, Body } from "@nestjs/common";
+
+@Controller('users')
+export class UserController {
+
+    @Post()
+    async create(@Body() body) {
+        return { body };
+    }
+}

7370691908f6992f4f60d3f329d7162e703a417a
DavidIsrael <davidisrael.rj@gmail.com>
2024-09-19 17:50:10 -0300

    metodo Get

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import {Controller, Post, Body } from "@nestjs/common";
+import {Controller, Post, Body, Get, Param } from "@nestjs/common";
 
 @Controller('users')
 export class UserController {
@@ -7,4 +7,15 @@ export class UserController {
     async create(@Body() body) {
         return { body };
     }
+
+    @Get('')
+    async read() {
+        return {users:[{'name':'David'}]}
+    }
+    
+    @Get(':id')
+    async readOne(@Param() param){
+        return {user:{}, param}
+    }
+
 }

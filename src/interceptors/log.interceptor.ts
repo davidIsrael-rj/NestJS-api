9714b1737deeac77618b036c39221c0e02ed044e
DavidIsrael <davidisrael.rj@gmail.com>
2024-09-24 21:31:17 -0300

    exibindo a url no console.log

--- src/interceptors/log.interceptor.ts
+++ src/interceptors/log.interceptor.ts
@@ -9,7 +9,12 @@ export class LogInterceptor implements NestInterceptor {
         const dt = Date.now();
 
         return next.handle().pipe(tap(()=>{
+
+            const request = context.switchToHttp().getRequest();
+
+            console.log(`URL: ${request.url}`)
             console.log(`Execução levou: ${Date.now() - dt} milisegundos`)
+
         }))
 
     }

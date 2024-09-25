607d7a7aa875be87a9cf585c65d438f3629cffd6
DavidIsrael <davidisrael.rj@gmail.com>
2024-09-24 21:33:05 -0300

    exibindo o method

--- src/interceptors/log.interceptor.ts
+++ src/interceptors/log.interceptor.ts
@@ -13,6 +13,7 @@ export class LogInterceptor implements NestInterceptor {
             const request = context.switchToHttp().getRequest();
 
             console.log(`URL: ${request.url}`)
+            console.log(`METHOD: ${request.method}`)
             console.log(`Execução levou: ${Date.now() - dt} milisegundos`)
 
         }))

69b2b57efa3efadfef7e8424bb8aa9d2bfa404ff
DavidIsrael <davidisrael.rj@gmail.com>
2024-09-30 19:56:03 -0300

    Montando a estrutura da autenticação

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,12 @@
 import { Module } from "@nestjs/common";
 import {JwtModule} from "@nestjs/jwt";
+import { AuthController } from "./auth.controller";
 
 @Module({
     imports:[JwtModule.register({
         secret:"!^Kt1:}jtz,.}}4~4Uhmi;U_T#pnA>n1"
-    })]
+    })],
+    controllers:[AuthController]
 })
 export class AuthModule{
 

51de40c641336d340e9e10b4724ebec57fae6f7f
DavidIsrael <davidisrael.rj@gmail.com>
2024-10-01 20:21:39 -0300

    AuthControle

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,19 @@
 import { Module } from "@nestjs/common";
-import {JwtModule} from "@nestjs/jwt";
+import { JwtModule } from "@nestjs/jwt";
 import { AuthController } from "./auth.controller";
+import { UserModule } from "src/user/user.module";
+import { PrismaModule } from "src/prisma/prisma.module";
 
 @Module({
-    imports:[JwtModule.register({
-        secret:"!^Kt1:}jtz,.}}4~4Uhmi;U_T#pnA>n1"
-    })],
-    controllers:[AuthController]
+    imports:
+        [JwtModule.register({
+            secret: "!^Kt1:}jtz,.}}4~4Uhmi;U_T#pnA>n1"
+        }),
+            UserModule,
+            PrismaModule
+        ],
+    controllers: [AuthController]
 })
-export class AuthModule{
+export class AuthModule {
 
 }

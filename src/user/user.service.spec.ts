c464bf49b081032ff7d408dda9ea39f9ddc33b03
DavidIsrael <davidisrael.rj@gmail.com>
2024-10-30 17:27:46 -0300

    Criando teste para o create

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -3,7 +3,16 @@ import { UserService } from "./user.service";
 import { getRepositoryToken } from "@nestjs/typeorm";
 import { UserEntity } from "./entity/user.entity";
 import { userRepositoryMock } from "../testing/user-repository.mock";
+import { CreateUserDTO } from "./dto/create-user.dto";
+import { Role } from "../enums/role.enum";
 
+const userEntityList: UserEntity[] =[{
+    birthAt:'2000-01-01',
+    email:'joao@admin.com.br',
+    name:'João',
+    password: '123456',
+    role: Role.User
+}]
 describe('UserService', () => {
 
     let userService: UserService;
@@ -22,5 +31,27 @@ describe('UserService', () => {
 
     test('Validar a definição', () => {
 
+    });
+
+    describe('Create', ()=>{
+
+        test('method create', async()=>{
+            const data:CreateUserDTO ={
+                birthAt:'2000-01-01',
+                email:'joao@admin.com.br',
+                name:'João',
+                password: '123456',
+                role: Role.User
+            }
+
+            const result = await userService.create(data);
+
+            expect(result).toEqual
+        })
+
     })
+    describe('Read', async ()=>{})
+    describe('Update', async ()=>{})
+    describe('Delete', async ()=>{})
+
 });

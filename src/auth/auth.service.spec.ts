c361c089530cf758afdc1babdf7a49cdcf578479
DavidIsrael <davidisrael.rj@gmail.com>
2024-11-03 20:46:08 -0300

    Test Validar a definição

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -3,18 +3,28 @@ import { AuthService } from "./auth.service"
 import { userRepositoryMock } from "../testing/user-repository.mock"
 import { jwtServiceMock } from "../testing/jwt-service.mock";
 import { UserServiceMock } from "../testing/user-service.mock";
+import { mailerServiceMock } from "../testing/mailer-service.mock";
 
-describe('AuthService', () =>{
+describe('AuthService', () => {
 
-    beforeEach(async ()=>{
+    let authService: AuthService;
+
+    beforeEach(async () => {
 
         const module: TestingModule = await Test.createTestingModule({
-           providers: [
-            AuthService,
-            userRepositoryMock,
-            jwtServiceMock,
-            UserServiceMock,
-        ] 
+            providers: [
+                AuthService,
+                userRepositoryMock,
+                jwtServiceMock,
+                UserServiceMock,
+                mailerServiceMock,
+            ]
         }).compile();
+
+        authService = module.get<AuthService>(AuthService);
     });
+
+    test('Validar a definição', () => {
+        expect(authService).toBeDefined();
+    })
 });

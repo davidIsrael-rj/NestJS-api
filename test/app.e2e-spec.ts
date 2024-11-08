3780a41f993eec381cbc7f747054f93828f579d6
DavidIsrael <davidisrael.rj@gmail.com>
2024-11-08 20:05:02 -0300

    Teste e2e - Registrar um novo usuário

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { authRegisterDTO } from '../src/testing/auth-register-dto.mock';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -24,5 +25,14 @@ describe('AppController (e2e)', () => {
       .get('/')
       .expect(200)
       .expect('Hello World!');
+  });
+
+  it('Registrar um novo usuário', async () => {
+    const response = await request(app.getHttpServer())
+    .post('/auth/register')
+    .send(authRegisterDTO);
+
+    expect(response.statusCode).toEqual(201);
+    expect(typeof response.body.accessToken).toEqual('string');
   });
 });
